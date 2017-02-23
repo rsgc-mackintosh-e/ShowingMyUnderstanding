58fbfd9115b5a95536e3f5b9775109c33af7312a
Euan Mackintosh <student@macbook.local>
2017-02-23 10:41:07 -0500

    Made sum stuf

--- ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Sequence - Practice.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Sequence - Practice.xcplaygroundpage/Contents.swift
@@ -28,10 +28,27 @@ import PlaygroundSupport
 
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
+canvas.drawShapesWithBorders = false
 
 // Replace this comment and add your code below...
 
+canvas.fillColor = Color(hue: 300, saturation: 100, brightness: 100, alpha: 50)
+canvas.drawEllipse(centreX: 75, centreY: 75, width: 75, height: 75)
+canvas.drawEllipse(centreX: 75, centreY: 225, width: 75, height: 75)
+canvas.drawEllipse(centreX: 150, centreY: 75, width: 75, height: 75)
 
+canvas.lineColor = Color(hue: 300, saturation: 100, brightness: 100, alpha: 100)
+canvas.drawLine(fromX: 75, fromY: 75, toX: 75, toY: 225, lineWidth: 75)
+canvas.drawLine(fromX: 75, fromY: 225, toX: 150, toY: 75, lineWidth: 75)
+
+canvas.fillColor = Color(hue: 122, saturation: 100, brightness: 100, alpha: 50)
+canvas.drawEllipse(centreX: 225, centreY: 75, width: 75, height: 75)
+canvas.drawEllipse(centreX: 225, centreY: 225, width: 75, height: 75)
+canvas.drawEllipse(centreX: 150, centreY: 75, width: 75, height: 75)
+
+canvas.lineColor = Color(hue: 122, saturation: 100, brightness: 100, alpha: 100)
+canvas.drawLine(fromX: 225, fromY: 75, toX: 225, toY: 225, lineWidth: 75)
+canvas.drawLine(fromX: 225, fromY: 225, toX: 150, toY: 75, lineWidth: 75)
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
