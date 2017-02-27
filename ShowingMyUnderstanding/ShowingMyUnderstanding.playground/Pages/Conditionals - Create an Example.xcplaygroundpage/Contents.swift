ee24b1f4d23aae68608d7484f8bdb2ce5172d79a
Euan Mackintosh <student@macbook.local>
2017-02-27 10:05:13 -0500

    Fixed stuff

--- ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
@@ -21,32 +21,32 @@ let canvas = Canvas(width: 300, height: 300)
 
 // Replace this command and add your code below...
 
-let number = random(from: 1, toButNotIncluding: 11)
+let x = x(from: 1, toButNotIncluding: 11){
 
-write " RSGC Test scores (Out of 10)"
+print( "RSGC Test scores (Out of 10)")
 
-if x = 10 write " Wow its a number 10!"
+if x = 10 print("Wow its a 10!")
 
-if x = 9 write " Pretty good, still a 9!"
+if x = 9 print("Pretty good, still a 9!")
 
-if x = 8 write " Still good!"
+if x = 8 print("Still good!")
 
-if x = 7 write " Meh"
+if x = 7 print("Meh")
 
-if x = 6 write " Come on man"
+if x = 6 print("Come on man")
 
-if x = 5 write " OK, you have to pick it up"
+if x = 5 print("OK, you have to pick it up")
 
-if x = 4 write " Oh god, this is not good"
+if x = 4 print("Oh god, this is not good")
 
-if x = 3 write " To knight support you go"
+if x = 3 print("To knight support you go")
 
-if x = 2 write " Im calling home"
+if x = 2 print("Im calling home")
 
-if x = 1 write " You are done, out of RSGC"
+if x = 1 print("You are done, out of RSGC")
 
 
-
+}
 
 /*:
  ## Template code
